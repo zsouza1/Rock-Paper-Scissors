6e134364288ae12eee0369ee1593b547e86eb376
zsouza1 <zsouza14@gmail.com>
2025-09-27 12:40:15 -0700

    Begin styling via DOM. RPS display logic broken.

--- script.js
+++ script.js
@@ -9,26 +9,26 @@ function getComputerChoice() {
     else return "scissors";
 }
 
-function getHumanChoice() {
-    return prompt("rock, paper or scissors?").toLowerCase();
-}
-
 function playRound(humanChoice, computerChoice) {
     console.log(`You chose: ${humanChoice}`);
     console.log(`Computer chose: ${computerChoice}`);
+    let result;
 
     if (humanChoice === computerChoice) {
-        console.log("It's a tie!");
+        result = "It's a tie!";
+        return result;
     } else if (
         (humanChoice === "rock" && computerChoice === "scissors") ||
         (humanChoice === "paper" && computerChoice === "rock") ||
         (humanChoice === "scissors" && computerChoice === "paper")
     ) {
-        console.log("You win!");
+        result = "You win!";
         humanScore++;
+        return result;
     } else {
-        console.log("You lose!");
+        result = "You lose!"
         computerScore++;
+        return result;
     }
 
     console.log(`Score - You: ${humanScore}, Computer: ${computerScore}`);
@@ -44,35 +44,92 @@ function gameOver() {
         console.log('A tie? That\'s unsatisfying...');
     }
 }
+//Begin styling via the DOM
+const div = document.createElement("div");
+document.body.appendChild(div);
 
+const choices = document.createElement("div");
+div.appendChild(choices);
+const choicesStyle = {
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center",
+    gap: "20px",
+    padding: "20px",
+    background: "blue"
+};
+Object.assign(choices.style, choicesStyle);
+
+const rockBox = document.createElement("div");
+choices.appendChild(rockBox);
+rockBox.setAttribute("style", "padding: 20px; background: orange;");
+
+const paperBox = document.createElement("div");
+choices.appendChild(paperBox);
+paperBox.setAttribute("style", "padding: 20px; background: green;");
+
+const scissorsBox = document.createElement("div");
+choices.appendChild(scissorsBox);
+scissorsBox.setAttribute("style", "padding: 20px; background: pink;");
+
+//Create buttons and group into buttons variable for condensed event listeners
 const rock = document.createElement("button");
 rock.textContent = "Rock";
-document.body.appendChild(rock);
+rockBox.appendChild(rock);
 
 const paper = document.createElement("button");
 paper.textContent = "Paper";
-document.body.appendChild(paper);
+paperBox.appendChild(paper);
 
 const scissors = document.createElement("button")
 scissors.textContent = "Scissors";
-document.body.appendChild(scissors);
+scissorsBox.appendChild(scissors);
 
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
     button.addEventListener("click", () =>{
-        
-        return button.textContent;
+        playRound(button.textContent.toLowerCase(), getComputerChoice());
 
 
     });
 });
 
-let buttonClicked = buttons.textContent
 
-document.getElementById(buttons).onclick = playRound(buttonClicked, computerChoice) {};
+//Scoreboard styling
+const scoreboard = document.createElement("div");
+document.body.appendChild(scoreboard);
+const scoreboardStyle = {
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center",
+    gap: "20px",
+    padding: "20px",
+    background: "yellow"
+};
+Object.assign(scoreboard.style, scoreboardStyle);
 
+const humanScoreDisplay = document.createElement("div");
+scoreboard.appendChild(humanScoreDisplay);
+humanScoreDisplay.style.padding = "20px";
+humanScoreDisplay.style.backgroundColor = "cyan";
+humanScoreDisplay.textContent = humanScore;
+
+const scoreboardMessage = document.createElement("div");
+scoreboard.appendChild(scoreboardMessage);
+// temporary styling, change into object later
+scoreboardMessage.style.padding = "20px";
+scoreboardMessage.style.fontSize = "25px";
+scoreboardMessage.textContent = "TEST";
+scoreboardMessage.style.backgroundColor = "red";
+
+const computerScoreDisplay = document.createElement("div");
+scoreboard.appendChild(computerScoreDisplay);
+computerScoreDisplay.style.padding = "20px";
+computerScoreDisplay.style.backgroundColor = "white";
+computerScoreDisplay.textContent = computerScore;
 
 gameOver();
 
 
 
+document.body.style.backgroundColor = "black";
